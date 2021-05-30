e44cea181e902d8932761ec810519a53173f8635
Yasniel Fajardo Egues <54322191+yasniel1408@users.noreply.github.com>
2021-05-30 14:42:26 -0400

    react helmet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Helmet } from "react-helmet";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Page1 />
+          </Route>
+          <Route path="/page2">
+            <Page2 />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+export const Page1 = () => (
+  <div>
+    <Helmet htmlAttributes>
+      <html lang="en" />
+      <meta charset="utf-8"></meta>
+      <title>Este es el titulo de la pagina 1</title>
+      <meta name="description" content="Description Page 1" />
+      <link rel="icon" href={logo} sizes="32x32" />
+    </Helmet>
+    <h1>Este es el titulo de la pagina 1</h1>
+    <Link to="/page2">ir a Pagina 2</Link>
+  </div>
+);
+
+export const Page2 = () => (
+  <div>
+    <Helmet htmlAttributes>
+      <html lang="en" />
+      <meta charset="utf-8"></meta>
+      <title>Este es el titulo de la pagina 2</title>
+      <meta name="description" content="Page 2" />
+      <link rel="icon" href={logo} sizes="32x32" />
+    </Helmet>
+    <h1>Este es el titulo de la pagina 2</h1>
+    <Link to="/">ir a Pagina 1</Link>
+  </div>
+);
